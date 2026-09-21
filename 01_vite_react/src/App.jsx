bd2b10214f1b3486eb9a1666efccb90a1b5c8255
Amisharanjan638 <amisharanjan43@gmail.com>
2026-09-21 10:41:13 +0530

    change

---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Chai from "./chai";
+
+
+function App() {
+  
+  return (
+<>
+    <Chai />
+    <h1>chai aur react with vite|Amisha Ranjan</h1>
+    <p> test para</p>
+    </>
+  )
+}
+
+export default App
